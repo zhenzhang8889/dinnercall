3b2f83d23cdb007215c4240b4b5766d86cc2f5a1
unknown <Tiger@.(none)>
2013-06-29 18:16:29 +0800

    first commit

---
+++ jquery/jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$(window).resize(function() {
+		var show_height = $(window).height();
+		var show_width = $(window).width();
+		consol.log(show_width);
+	});
+
+}); 
